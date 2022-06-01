eb9cff70966b9a2bfc3751832c2f6043af8a5a0b
Damian-Sommer <damian@sommer5.ch>
2022-06-01 07:58:53 +0200

    Create Column Model

---
+++ src/main/java/ch/aeltict/dbinput/logic/Column.java
@@ -0,0 +1,36 @@
+package ch.aeltict.dbinput.logic;
+
+public class Column{
+
+    private String columnName;
+    private String tableName;
+    private int columnIndex;
+    private String columnDataType;
+
+
+    public Column(String columnName, int columnIndex, String tableName){
+        this.columnName = columnName;
+        this.columnIndex = columnIndex;
+        this.tableName = tableName;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public int getColumnIndex() {
+        return columnIndex;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public String getColumnDataType() {
+        return columnDataType;
+    }
+
+    public void setColumnDataType(String columnDataType) {
+        this.columnDataType = columnDataType;
+    }
+}
